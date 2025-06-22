452511fb28298c02c325c53acd567e955fbd09c5
szdk <34851053+szdk@users.noreply.github.com>
2025-06-22 22:28:45 +0530

    Create common-functions.py

---
+++ common-functions.py
@@ -0,0 +1,23 @@
+def extractZip(zip_path, extract_to):
+  if not os.path.isdir(extract_to):
+    print("Extracting zip")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+    print("Extraction complete.")
+  else:
+    print("Directory already exists.")
+
+def loadFiles(directory):
+  file_list = [
+      os.path.join(directory, f)
+      for f in os.listdir(directory)
+      if os.path.isfile(os.path.join(directory, f))
+  ]
+  return file_list
+
+def downloadRemote(remote_url, download_to):
+  import requests
+  response = requests.get(remote_url)
+  with open(download_to, "w") as f:
+      f.write(response.text)
+  return download_to
