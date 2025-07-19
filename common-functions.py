5896ddea09041472b0628aa04cfb5f7c4599cd53
szdk <34851053+szdk@users.noreply.github.com>
2025-07-19 18:26:24 +0530

    Update common-functions.py

--- common-functions.py
+++ common-functions.py
@@ -6,6 +6,7 @@ import os
 import requests
 from tqdm.notebook import tqdm
 import math
+import random
 
 def extractZip(zip_path, extract_to):
   if not os.path.isdir(extract_to):
@@ -71,5 +72,81 @@ def trainModel(
             cur_batch_x = []
             cur_batch_y = []
             pbar.update(batch_size)
+            print(f"\rLoss: {loss.item():.4f}", end="")
+        pbar.close()
+
+
+def normalize_batchwise(X, feature_range=(0.0, 1.0)):
+    # X shape: [batch_size, 1, 1024, 6]
+    
+    # Keep dims for broadcasting: reduce dims 1,2,3 (channel, time, feature)
+    min_vals = X.amin(dim=(1, 2, 3), keepdim=True)  # shape: [batch_size, 1, 1, 1]
+    max_vals = X.amax(dim=(1, 2, 3), keepdim=True)  # shape: [batch_size, 1, 1, 1]
+    
+    scale = feature_range[1] - feature_range[0]
+    X_norm = (X - min_vals) / (max_vals - min_vals + 1e-8)
+    X_norm = X_norm * scale + feature_range[0]
+    
+    return X_norm
+
+
+
+def getones(window_size, trainTensor):
+    ones = []
+    for i in range(window_size, len(trainTensor)):
+        if (trainTensor[i, -1] > 0.9):
+            ones.append(trainTensor[i-window_size:i, :6].unsqueeze(0))
+    return ones
+
+#equalizes by signal (single predection)
+def trainModelBalancedSingle(
+    files,
+    model,
+    criterion,
+    optimizer,
+    batch_size,
+    window_size,
+    train_split_perc,
+    device
+):
+    model.train()
+    for file in tqdm(files):
+        df = pd.read_csv(file).iloc[:, 1:]
+        fullTensor = torch.tensor(df.values).float().to(device)
+        trainTensor = fullTensor[:math.floor(len(fullTensor)*train_split_perc)]
+        ones = getones(window_size, trainTensor)
+        print(f"\nLoaded {df.shape} from {file}")
+        print(f"fullTensor: {fullTensor.shape}, trainTensor: {trainTensor.shape}")
+        cur_batch_x = []
+        cur_batch_y = []
+        batches_trained = 0
+
+        end = len(trainTensor)
+        pbar = tqdm(total=(end-window_size))
+        i = window_size - 1
+        while i < end:
+          i += 1
+          if (trainTensor[i, -1].item() > 0.9):
+             continue
+          if (random.random() < 0.5):
+            cur_sample = trainTensor[i-window_size:i, :6].unsqueeze(0)
+            cur_batch_y.append(torch.tensor([0.0], device=device, dtype=torch.float32))
+          else:
+            cur_sample = random.choice(ones).clone()
+            cur_batch_y.append(torch.tensor([1.0], device=device, dtype=torch.float32))
+            i -= 1
+          cur_batch_x.append(cur_sample)
+          if len(cur_batch_x) == batch_size or i == end-1:
+            batches_trained += 1
+            X = torch.stack(cur_batch_x).to(device)
+            Y = torch.stack(cur_batch_y)
+            outputs = model(normalize_batchwise(X))
+            loss = criterion(outputs, Y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            pbar.update(len(cur_batch_x))
+            cur_batch_x = []
+            cur_batch_y = []
             print(f"\rLoss: {loss.item():.4f}", end="")
         pbar.close()
